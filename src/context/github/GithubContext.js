60e3a7eb2bc72714aeff9251b9345a2c41c12a30
Tatiana Paga Franch <tatianapagafranch@gmail.com>
2022-03-10 22:53:56 +0100

    clear button now works

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -36,6 +36,10 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  // Clear the users from state
+
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
+
   // Set loading
   const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
@@ -45,6 +49,7 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         searchUsers,
+        clearUsers,
       }}
     >
       {children}
